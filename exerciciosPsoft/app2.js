5839a25ad014e26253335e7ebb1161e25ced6950
erickems <erick.sena@ccc.ufcg.edu.br>
2020-05-04 18:44:13 -0300

    "Melhorando a resposta do exercício"

--- exerciciosPsoft/app2.js
+++ exerciciosPsoft/app2.js
@@ -1,86 +1,76 @@
 
 let myRequest =  new Request("./dados.json")
 let form = document.querySelector("form")
+let button = document.querySelector("#button").addEventListener("click", getValue)
 let inputRadio = ""
 let jsonValor = ""
+let arrayDoencas
+let arraySintomas
 let click = 0
-let random 
-let resultadoJson
+let dica = 1 //indice da dica. A dica[0] já foi usada na inicializacao
+let randomDoenca
 
-async function carregaJson(){
+async function carregaPagina(){
     let json = await (await fetch(myRequest)).json()
-    resultadoJson = json
-
-    random = parseInt((Math.random()*10)%4)
-
-    switch(random){
-        case 0:
-            document.querySelector("#dica1").innerText = "Dica 1: " + json.covid[0]
-            break
-        case 1:
-            document.querySelector("#dica1").innerText = "Dica 1: " + json.gripe[0]
-            break
-        case 2:
-            document.querySelector("#dica1").innerText = "Dica 1: " + json.dengue[0]
-            break
-        default:
-            document.querySelector("#dica1").innerText = "Dica 1: " + json.alergia[0]
-            break
-    }
-
+    
+    arrayDoencas = json  
+    randomDoenca = parseInt((Math.random() * 10) % arrayDoencas.length) //escolhi o indice da doença
+    
+    start()
 }
-carregaJson()
+
+function start(){
+    arraySintomas = arrayDoencas[randomDoenca].dicas  //peguei o array de sintomas da doenca escolhida
+    document.querySelector("#dica1").innerText = "Dica 1: " + arraySintomas[0] // sintoma 1
+}
+
+function selecionaNovaDoenca(){
+    for(let i = 1; i <= 4; i++){
+        document.querySelector(`#dica${i}`).innerText = ""
+    }
+    document.querySelector("#acertou").innerText = ""
+
+    arrayDoencas.splice(randomDoenca, 1)
+    randomDoenca = parseInt((Math.random() * 10) % arrayDoencas.length) //escolhi o indice da nova doença
+    start()
+}
 
 function getValue(){
-
     let data = new FormData(form)
     
     for (const f of data){
         inputRadio = f[1]
     }
-    
-    switch(random){
-        case 0:
-            jsonValor = "Covid-19"
-            verifica(resultadoJson.covid)
-            break
-        case 1:
-            jsonValor = "Gripe"
-            verifica(resultadoJson.gripe)
-            break
-        case 2:
-            jsonValor = "Dengue"
-            verifica(resultadoJson.dengue)
-            break
-        case 3:
-            jsonValor = "Alergia"
-            verifica(resultadoJson.alergia)
-            break
-    }
+
+    jsonValor = arrayDoencas[randomDoenca].nome
+    verifica(arrayDoencas[randomDoenca].dicas)
+
     click++
 }
 
-function verifica(json){
+function verifica(dicas){
+    let sintoma = dicas[dica]
+    let queryId = `#dica${dica+1}`
+    
     if ((inputRadio != jsonValor) && click < 3){
-        switch(click){
-            case 0:
-                document.querySelector("#dica2").innerText = "Dica 2: " + json[1]
-                break
-            case 1:
-                document.querySelector("#dica3").innerText = "Dica 3: " + json[2]
-                break
-            default:
-                document.querySelector("#dica4").innerText = "Dica 4: " + json[3]
-                break
-        }
+        document.querySelector(queryId).innerText = "Dica " + (dica+1) + ": " + sintoma
     }
     else if (inputRadio == jsonValor){
         document.querySelector("#acertou").innerText = "Você acertou!"
-        setTimeout(()=>{document.location.reload()},700)
-        
+        setTimeout(()=>{
+            selecionaNovaDoenca()
+            click = 0
+            dica = 1
+        },700)
     }
     else if ((inputRadio != jsonValor) && click >= 3){
         document.querySelector("#acertou").innerText = "Acabou as dicas. Você Errou!"
-        setTimeout(()=>{document.location.reload()},700)
+        setTimeout(()=>{
+            selecionaNovaDoenca()
+            click = 0
+            dica = 1
+        },700)
     }
+    dica += 1
 }
+carregaPagina()
