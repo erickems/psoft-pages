8d0506a4dd48fc96514347827289ec0b177978a3
erickems <erick.sena@ccc.ufcg.edu.br>
2020-05-06 18:20:32 -0300

    "chuck0"

--- homepage/chuck0/app.js
+++ homepage/chuck0/app.js
@@ -5,13 +5,17 @@ document.querySelector("#button").addEventListener("click", click)
 
 async function main(){
     dados = await (await fetch("dados.json")).json()
-    console.log(dados[index].value.joke)
 }
 main()
 
 function click(){
-    document.querySelector("#piada").innerText = dados[index].value.joke
-    index++
+    if(index < 20){
+        document.querySelector("#piada").innerText = dados[index].value.joke
+        index++
+    }
+    else{
+        document.querySelector("#piada").innerText = "cabou as piada"
+    }
 }
 
 
